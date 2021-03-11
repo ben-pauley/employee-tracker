c30378923d41925ec54125be204eb7afab7dbdd4
Ben <pauleybj@gmail.com>
2021-03-11 10:58:18 +0000

    connection to db

---
+++ utils/Database.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql");
+
+const DBNAME = "employee_db";
+const HOST = "localhost";
+const PORT = 3001;
+const USER = "root";
+const PASS = "root";
+
+const connection = mysql.createConnection({
+  host: HOST,
+  port: PORT,
+  user: USER,
+  password: PASS,
+  database: DBNAME,
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+});
+
+module.exports = connection;
